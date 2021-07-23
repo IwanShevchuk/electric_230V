2bda14d7100e6654e6b8bda55ee9ef6cc3db98ce
Ivan Shevchuk <41307731+IwanShevchuk@users.noreply.github.com>
2021-07-23 11:22:52 +0300

    Add files via upload

---
+++ theme-switcher1.js
@@ -0,0 +1,17 @@
+/* Created By Joseph Shenton @TeamiHackify 
+
+Please feel free to use this toggle if you like. But I request that you give credit to me thank you */
+
+ $('body').toggleClass('day-background');
+
+ $('#cb1').on('click', function () { $('body').toggleClass('day-background'); });
+
+
+// $('#three1').toggleClass('hiddens');
+// $('#three1').on('click', function () { $('card').toggleClass('hiddens'); });
+
+$('#cb2').on('click', function () { $('#two2').toggleClass('hiddens'); });
+$('#cb3').on('click', function () { $('#three3').toggleClass('hiddens'); });
+$('#cb4').on('click', function () { $('#four4').toggleClass('hiddens'); });
+$('#cb5').on('click', function () { $('#five5').toggleClass('hiddens'); });
+   // $('#three1').toggleClass('hiddens'); five5
